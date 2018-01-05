d81805d20b2c2db3a23b1bb6459ffb96af9f02b2
Miguel Juárez <mjuarez@yammer-inc.com>
2018-01-05 15:00:07 -0800

    Removed findById in favor of kotlin extensions (that makes use of a cache)

--- app/src/main/java/murki/githubexplorer/ui/MainFragment.kt
+++ app/src/main/java/murki/githubexplorer/ui/MainFragment.kt
@@ -3,10 +3,10 @@ package murki.githubexplorer.ui
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_main.mainRecyclerView
 
 import murki.githubexplorer.R
 import murki.githubexplorer.viewmodel.RepoItemVM
@@ -16,14 +16,13 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view : View = inflater.inflate(R.layout.fragment_main, container, false)
-
-        val recyclerView = view.findViewById(R.id.main_recycler_view) as RecyclerView
-
-        recyclerView.setHasFixedSize(true)
-        recyclerView.adapter = MainAdapter(listOf(RepoItemVM("name", "description")))
-
-        return view
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        mainRecyclerView.setHasFixedSize(true)
+        mainRecyclerView.adapter = MainAdapter(listOf(RepoItemVM("name", "description")))
+    }
 }
