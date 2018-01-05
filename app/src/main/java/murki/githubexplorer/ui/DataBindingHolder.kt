d15ccfd48ab4721a814ac0d490d4ca325d0a4d0a
Miguel Juárez <mjuarez@yammer-inc.com>
2018-01-05 14:17:29 -0800

    Made DataBindingHolder generic to get type safety

--- app/src/main/java/murki/githubexplorer/ui/DataBindingHolder.kt
+++ app/src/main/java/murki/githubexplorer/ui/DataBindingHolder.kt
@@ -5,8 +5,8 @@ import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
 import android.view.View
 
-class DataBindingHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class DataBindingHolder<out T : ViewDataBinding>(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    val binding: ViewDataBinding = DataBindingUtil.bind(itemView)
+    val binding: T = DataBindingUtil.bind(itemView)
 
 }
--- app/src/main/java/murki/githubexplorer/ui/MainAdapter.kt
+++ app/src/main/java/murki/githubexplorer/ui/MainAdapter.kt
@@ -7,18 +7,15 @@ import murki.githubexplorer.R
 import murki.githubexplorer.databinding.RepoItemBinding
 import murki.githubexplorer.viewmodel.RepoItemVM
 
-class MainAdapter(var dataset : List<RepoItemVM>) : RecyclerView.Adapter<DataBindingHolder>() {
+class MainAdapter(var dataset : List<RepoItemVM>) : RecyclerView.Adapter<DataBindingHolder<RepoItemBinding>>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): DataBindingHolder {
-        // create a new view
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): DataBindingHolder<RepoItemBinding> {
         val view = LayoutInflater.from(parent?.context).inflate(R.layout.repo_item, parent, false)
-
         return DataBindingHolder(view)
     }
 
-    override fun onBindViewHolder(holder: DataBindingHolder?, position: Int) {
-        val binding = holder?.binding as RepoItemBinding
-        binding.viewModel = dataset[0]
+    override fun onBindViewHolder(holder: DataBindingHolder<RepoItemBinding>?, position: Int) {
+        holder?.binding?.viewModel = dataset[0]
     }
 
     override fun getItemCount(): Int {
