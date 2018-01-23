8ed095c6eb38c224b5d171b00792f0a3617c3e40
Miguel Juárez <mjuarez@yammer-inc.com>
2018-01-22 19:04:31 -0800

    Cleaned-up the DataBindViewHolder to not inflate but receive tha inflated binding in a dataclass

--- app/src/main/java/murki/githubexplorer/ui/DataBindingHolder.kt
+++ app/src/main/java/murki/githubexplorer/ui/DataBindingHolder.kt
@@ -1,12 +1,6 @@
 package murki.githubexplorer.ui
 
-import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
-import android.view.View
 
-class DataBindingHolder<out T : ViewDataBinding>(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    val binding: T = DataBindingUtil.bind(itemView)
-
-}
+data class DataBindingHolder<out T : ViewDataBinding>(val binding: T) : RecyclerView.ViewHolder(binding.root)
--- app/src/main/java/murki/githubexplorer/ui/MainAdapter.kt
+++ app/src/main/java/murki/githubexplorer/ui/MainAdapter.kt
@@ -1,20 +1,29 @@
 package murki.githubexplorer.ui
 
 import android.support.v7.recyclerview.extensions.ListAdapter
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import murki.githubexplorer.R
 import murki.githubexplorer.databinding.RepoItemBinding
 import murki.githubexplorer.viewmodel.RepoItemVM
 
 class MainAdapter : ListAdapter<RepoItemVM, DataBindingHolder<RepoItemBinding>>(RepoItemDiff()) {
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): DataBindingHolder<RepoItemBinding> {
-        val view = LayoutInflater.from(parent?.context).inflate(R.layout.repo_item, parent, false)
-        return DataBindingHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBindingHolder<RepoItemBinding> {
+        Log.d(CLASSNAME, "onCreateViewHolder() called. Inflating view for row. viewType=$viewType")
+        val binding: RepoItemBinding = RepoItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DataBindingHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: DataBindingHolder<RepoItemBinding>?, position: Int) {
-        holder?.binding?.viewModel = getItem(position)
+    override fun onBindViewHolder(holder: DataBindingHolder<RepoItemBinding>, position: Int) {
+        Log.d(CLASSNAME, "onBindViewHolder() called. Binding data to view. hasPendingBindings=${holder.binding.hasPendingBindings()}")
+        holder.binding.viewModel = getItem(position)
+        if (holder.binding.hasPendingBindings()) {
+            holder.binding.executePendingBindings()
+        }
+    }
+
+    companion object {
+        private const val CLASSNAME = "MainAdapter"
     }
 
 }
